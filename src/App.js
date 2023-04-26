4e2e3fe987ea5ecf869720e1dc38d1867cced10b
Yuliya <yulua03071992@gmail.com>
2023-04-26 16:59:30 +0300

    add Setup and Mock Products

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Products from './components/Products/Products';
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
